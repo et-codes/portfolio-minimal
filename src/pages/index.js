d4cedc1a2efd74d977d88abfd99d270eb6e6ce9f
Eric Thornton <ericthornton43@gmail.com>
2022-09-13 13:26:50 -0600

    Update Hero section, header, and footer.

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,11 +16,11 @@ export default function IndexPage() {
       <Seo title="Gatsby Starter for Portfolio Minimal" />
       <Page useSplashScreenAnimation>
         <HeroSection sectionId="hero" />
-        <ArticlesSection sectionId="articles" heading="Latest Articles" sources={['Medium']} />
+        {/* <ArticlesSection sectionId="articles" heading="Latest Articles" sources={['Medium']} /> */}
         <AboutSection sectionId="about" heading="About Portfolio Minimal" />
-        <InterestsSection sectionId="details" heading="Details" />
-        <ProjectsSection sectionId="features" heading="Built-in Features" />
-        <ContactSection sectionId="github" heading="Issues?" />
+        <InterestsSection sectionId="skills" heading="Technologies / Skills" />
+        <ProjectsSection sectionId="projects" heading="Projects" />
+        <ContactSection sectionId="contact" heading="Contact Me" />
       </Page>
     </>
   );
